d1554497ed0b11d200f86548c7f8fd75263907d0
vvedeshkin <evilfunt@gmail.com>
2018-09-04 21:19:47 +0300

    Release the first method of calculation without threads

--- src/com/github/vedeshkin/Main.java
+++ src/com/github/vedeshkin/Main.java
@@ -1,8 +1,34 @@
 package com.github.vedeshkin;
 
+import java.util.Arrays;
+
 public class Main {
 
+    static final int size = 10000000;
+    static final int h = size / 2;
+    float data[] = new float[size];
+    float defaultValue = 1.0f;
+
     public static void main(String[] args) {
+        new Main().calculate();
+        new Main().concurrentCalculate();
+    }
+
+    private void calculate() {
+
+        Arrays.fill(data,defaultValue);
+        long start  = System.currentTimeMillis();
+        for (int i = 0; i< data.length;i++)
+        {
+            data[i] = (float)(data[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken : " + (end - start));
+    }
+
+    private void concurrentCalculate() {
+
+
 
     }
 }
